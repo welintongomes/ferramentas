4e0251d21fb998bc51a600490b8f7d07d5da901e
welintongomes <welintongm@gmail.com>
2025-06-30 22:22:36 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meu-site-cache-v1.4';
+const CACHE_NAME = 'meu-site-cache-v1.6';
 const urlsToCache = [
     './', // Cacheia o arquivo HTML principal
     //'./bootstrap.min.css',
